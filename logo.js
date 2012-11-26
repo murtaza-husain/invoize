cd55b5f1253107b45a2b5de2a7ab2f0ff89ad236
Vishal Patel <vishalpatel2374@gmail.com>
2012-11-26 14:32:47 +0530

    change css, wizard, add processing image

---
+++ logo.js
@@ -0,0 +1,50 @@
+		function logorun()
+		{
+			var urlval1=document.URL.search("searce.invoize.org");
+			var urlval2=document.URL.search("ceva.invoize.org");
+			if (urlval1>0)
+			{
+				
+				document.getElementById("logo1").style.display="block";
+				document.getElementById("Reg_link").style.display="none";
+				
+			}
+			else if(urlval2>0)
+			{
+				document.getElementById("logo2").style.display="block";
+				document.getElementById("Reg_link").style.display="none";
+			}
+			else
+			{
+				document.getElementById("Reg_link").style.display="block";
+			}
+		}
+		function goto()
+		{
+			
+			var urlval=document.URL.search("searce.invoize.org");
+			var urlval2=document.URL.search("ceva.invoize.org");
+			
+			if(urlval>0)
+			{
+				window.location.assign("http://searce.invoize.org/prototype");
+			}
+			else if(urlval2>0)
+			{
+				window.location.assign("http://ceva.invoize.org/prototype");
+			}
+			else
+			{
+				window.location.assign("http://abc.invoize.org/prototype");
+			}
+		}	
+		function signup()
+		{
+			document.getElementById("registration").style.display="block";
+		}
+		function Cancel_signup()
+		{
+			document.getElementById("registration").style.display="none";
+		}
+		
+	
